eeb0294e271f526d68bee6911d7137a8cc829153
QuasarRay <quvazaray@gmail.com>
2026-06-11 15:24:16 +1000

    fixed tonic api version mismatch error

--- distributed-backend/src/trade-settlement/build.rs
+++ distributed-backend/src/trade-settlement/build.rs
@@ -3,9 +3,16 @@
 // Keeping code generation in build.rs prevents hand-written transport structs
 // from drifting away from the proto API used by market.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().compile_protos(
-        &["../proto/settlement/v1/settlement.proto"],
-        &["../proto"],
+    tonic_prost_build::configure().compile_protos(
+        &[
+            "../../proto/trade/v1/common.proto",
+            "../../proto/trade/v1/market_types.proto",
+            "../../proto/trade/v1/operation.proto",
+            "../../proto/trade/v1/ownership.proto",
+            "../../proto/trade-settlement/v1/trade-settlement.proto",
+        ],
+        &["../../proto"],
     )?;
+
     Ok(())
 }
